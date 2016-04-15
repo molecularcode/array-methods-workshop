a94db6572677e77f9e6a11975b8a4e3699df1de9
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-15 18:26:55 +0000

    Exercise 4 - use .reduce to return longest word in a string

--- answers.js
+++ answers.js
@@ -26,5 +26,27 @@ function getPositives(number) {
 }
 
 var positiveNums = numbers2.filter(getPositives);
-console.log(positiveNums);
+//console.log(positiveNums);
 
+// Exercise 3 - Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+    // NOTE: You are allowed to use Array.prototype.filter to answer this question.
+    // NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
+    
+var mixed = [true, false, 0, NaN, 4, "yes", "no", undefined];
+
+var truthy = mixed.filter(Boolean);
+//console.log(truthy);
+
+
+// Exercise 4 - Write a function called longestWord that takes a string as argument, and returns the longest word in the string. You should use Array.prototype.reduce to do your work.
+    // Hint: You can use String.prototype.split to split the string into an array of words.
+
+function longestWord(string) {
+    var wordArray = string.split(" ");  
+    var longestWord = wordArray.reduce(function(longest, curr) {
+        return curr.length > longest.length ? curr : longest;
+    }, "");
+    return longestWord;
+}
+
+console.log(longestWord("Happiness is a method of life, not a destination"));
