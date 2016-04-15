2146cf1f20205a71a73e40230918a3eab850d3fa
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-15 17:26:36 +0000

    Exercise 1 - use .forEach to return the positive numbers

---
+++ answers.js
@@ -0,0 +1,4 @@
+// Day 5 - JS - DecodeMTL Array methods workshop
+
+// Exercise 1 -Write a function called printPositives that takes an array and uses its forEach method to print only the positive numbers.
+
